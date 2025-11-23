82c4bb22cbfee150a8042b36cfe5690dd4c4d706
Naylane <sardinhajuliana0@gmail.com>
2025-11-23 18:16:56 -0300

    Listagem de produtos

--- src/main/java/org/gerenciamentoestoque/database/ProdutoService.java
+++ src/main/java/org/gerenciamentoestoque/database/ProdutoService.java
@@ -2,14 +2,11 @@ package org.gerenciamentoestoque.database;
 
 import org.gerenciamentoestoque.models.Produto;
 
-import javax.sound.sampled.Port;
 import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class ProdutoService {
@@ -20,7 +17,23 @@ public class ProdutoService {
         this.conexao = conexao;
     }
 
-    public Produto inserirProduto() {
+    public Produto inserirProduto(Produto produto) {
+        try {
+            Statement statement = this.conexao.createStatement();
+            ResultSet resultSet = statement.executeUpdate("insert into produtos (nome, quantidade)");
+            retorno = new ArrayList<>();
+
+            while (resultSet.next()) {
+                int id = resultSet.getInt("ID");
+                String nome = resultSet.getString("nome");
+                int quantidade = resultSet.getInt("quantidade");
+
+                Produto produtinho = new Produto(id, nome, quantidade);
+                retorno.add(produtinho);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
         return null;
     }
 
@@ -28,7 +41,7 @@ public class ProdutoService {
         List<Produto> retorno;
         try {
             Statement statement = this.conexao.createStatement();
-            ResultSet resultSet = statement.executeQuery("select * from produtos");
+            ResultSet resultSet = statement.executeQuery("select id, nome, quantidade from produtos");
             retorno = new ArrayList<>();
 
             while (resultSet.next()) {
@@ -45,4 +58,6 @@ public class ProdutoService {
 
         return retorno;
     }
+
+
 }
--- src/main/java/org/gerenciamentoestoque/models/Produto.java
+++ src/main/java/org/gerenciamentoestoque/models/Produto.java
@@ -9,6 +9,11 @@ public class Produto {
     public Produto() {
     }
 
+    public Produto(String nome, int quantidade) {
+        this.nome = nome;
+        this.quantidade = quantidade;
+    }
+
     public Produto(int id, String nome, int quantidade) {
         this.id = id;
         this.nome = nome;
